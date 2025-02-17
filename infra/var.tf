6451877c9a9d22089e5ad56c19058a79f1bab7ae
ck <ck@ck.local>
2025-02-17 08:47:08 +0530

    infra code has been aded

---
+++ infra/var.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-12345678" # Change this to a valid AMI ID
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security Group ID for the EC2 instance"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the EC2 instance will be launched"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Tag Name for the EC2 instance"
+  type        = string
+  default     = "MyTerraformInstance"
+}
