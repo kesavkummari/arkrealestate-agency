b98ee8690655e4a7489060b7746a3e42cd9568f3
ck <ck@ck.local>
2025-02-17 08:56:14 +0530

    scripts

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "web" {
   key_name               = var.key_name
   vpc_security_group_ids = [var.security_group_id]
   subnet_id              = var.subnet_id
+  user_data              = file("install.sh")
 
   tags = {
     Name = var.instance_name
